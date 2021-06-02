56531bc95e811f5dffb6fc1c72a10d6ae62ef449
Dariusz Kalinowski <dariusz.kalinowski@1und1.de>
2021-06-02 14:55:47 +0200

    Added animations

--- app/src/main/java/com/uimam/compose/MainAcitvity.kt
+++ app/src/main/java/com/uimam/compose/MainAcitvity.kt
@@ -4,22 +4,35 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.animation.core.SpringSpec
+import androidx.compose.animation.core.TweenSpec
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.indication
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Button
 import androidx.compose.material.Card
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -31,6 +44,7 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.navArgument
 import androidx.navigation.compose.rememberNavController
 import com.example.compose.jetchat.R
+import com.google.accompanist.glide.rememberGlidePainter
 
 class MainAcitvity : AppCompatActivity() {
 
@@ -65,10 +79,30 @@ class MainAcitvity : AppCompatActivity() {
     }
 
     @Composable
-    fun HelloContent(list: List<Row>, onRowClicked: (Row) -> Unit) {
-        LazyColumn {
-            for (row in list) {
-                item { ARow(row, onRowClicked) }
+    fun HelloContent(list: List<Row>, onRowClicked: (Row) -> Unit, onButtonClicked: () -> Unit = {}) {
+        var enabled by remember { mutableStateOf(false) }
+        val alpha: Float by animateFloatAsState(if (enabled) 1f else 0f, animationSpec = TweenSpec(3000))
+        val size by animateDpAsState(if (enabled) 200.dp else 100.dp, animationSpec = TweenSpec(3000))
+
+        Column() {
+            LazyColumn(modifier = Modifier.height(100.dp)) {
+                for (row in list) {
+                    item { ARow(row, onRowClicked) }
+                }
+            }
+            Surface(
+                modifier = Modifier
+                    .alpha(alpha)
+                    .size(size)
+            ) {
+                Button(onClick = { enabled = !enabled }) {
+                    Image(
+                        painter = rememberGlidePainter(
+                            request = "https://picsum.photos/300/300"
+                        ),
+                        contentDescription = "",
+                    )
+                }
             }
         }
     }
